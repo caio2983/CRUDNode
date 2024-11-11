f9f56c1cf59cea93db2de552cc921c1a21fcce1c
caio2983 <caioluna2002@gmail.com>
2024-11-11 11:56:55 -0300

    update test

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { randomUUID } = require("crypto");
+const pool = require("./database/db.js");
+const port = 3000;
+
+const app = express();
+app.use(express.json());
+
+// Returns the entire table
+app.get("/", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM orders");
+    res.status(200).send(data.rows);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+// Creates an order
+app.post("/order", async (req, res) => {
+  const id = randomUUID();
+  const amount = Math.round(Math.random() * 5000);
+  try {
+    await pool.query("INSERT INTO orders(id, amount) VALUES ($1, $2)", [
+      id,
+      amount,
+    ]);
+    res.status(200).send({ message: "successfully added child" });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+// Sets up the db
+app.get("/setup", async (req, res) => {
+  try {
+    await pool.query("CREATE TABLE orders(id UUID PRIMARY KEY, amount INT )");
+    res.status(200).send({ message: "successfully created table" });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+module.exports = app;
---
+++ tests/get.test.js
@@ -0,0 +1,37 @@
+// docker exec -it 565698501ae1  /bin/bash
+// npm run test
+
+const request = require("supertest");
+const app = require("../app.js");
+const pool = require("../database/db.js");
+
+describe("GET /", () => {
+  beforeAll(async () => {
+    await pool.query(
+      "CREATE TABLE IF NOT EXISTS orders(id UUID PRIMARY KEY, amount INT)"
+    );
+    await pool.query("INSERT INTO orders (id, amount) VALUES ($1, $2)", [
+      "123e4567-e89b-12d3-a456-426614174000",
+      1000,
+    ]);
+  });
+
+  afterAll(async () => {
+    await pool.query("DELETE FROM orders");
+    await pool.query("DROP TABLE IF EXISTS orders");
+    await pool.end();
+  });
+
+  it("deve retornar uma lista de pedidos (orders)", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.status).toBe(200);
+
+    expect(Array.isArray(response.body)).toBe(true);
+
+    expect(response.body.length).toBeGreaterThan(0);
+
+    expect(response.body[0]).toHaveProperty("id");
+    expect(response.body[0]).toHaveProperty("amount");
+  });
+});
